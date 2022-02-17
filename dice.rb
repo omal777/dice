c3d063682f2c8b1a350c26dcbb8257c9c345c3ed
omal777 <egorlg3gs@gmail.com>
2022-02-17 14:12:26 +0200

    ver 1.3 выводит сумму бросков и вычищает экран

--- dice.rb
+++ dice.rb
@@ -1,9 +1,14 @@
 puts 'сколько кубиков кинуть'
 answ = gets.to_i
+sum = 0
+  system "clear" or system "cls"
 answ.times do
-100.times do
+30.times do
 print "#{rand(6) + 1}\r"
   sleep 0.011
 end
-puts rand(1..6)
+dice = rand(1..6)
+puts dice.to_s
+sum += dice
 end
+puts "Ваше количество очков: #{sum}"
